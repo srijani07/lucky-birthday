9cd9c939b37da3d3b3ba7099ab7ee475513b138f
srijani07 <srijaniparui07@gmail.com>
2021-08-02 13:11:18 +0530

    styling : added graphic output, alignment and updated font

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-var date = document.querySelector('#userinputdate');
+var date = document.querySelector('#userinput-date');
 var btnCheck = document.querySelector('#btn-check');
-
+var imgHappy = document.querySelector('#happy-img');
+var imgSad = document.querySelector('#sad-img');
 var number = document.querySelector
 ('#userinput-number');
 var output = document.querySelector('#output-area');
 
+
 function clickHandler(){
-    var userDate = date.value;
+
+    var birthDate = new Date(date.value);
     var userNumber = parseInt(number.value);
-    var elementYear = parseInt(userDate.slice(0,4));
-    var elementMonth = parseInt(userDate.slice(5,7));
-    var elementDate = parseInt(userDate.slice(9,11));
 
-    if(userNumber === elementYear+elementMonth+elementDate)
-output.innerHTML = "Yayyyy!! Your birthday is lucky!";
+    var elementYear = birthDate.getFullYear();
+    var elementMonth =birthDate.getMonth();
+    var elementDate = birthDate.getDate();
+    var sum = elementYear+elementMonth+elementDate+1;
 
-else 
-output.innerHTML = "Sorry. Your birthday is not lucky :(";
+if(sum === userNumber) {
+output.innerHTML = "Yayyy!! You have a lucky birthday!";
+imgSad.style.display = "none"; //resetting the display state (if user gets not lucky output first and then lucky, the state is updated)
+imgHappy.style.display = "inline";
+
 }
 
+else if(sum !== userNumber) {
+output.innerHTML ="Sorry! You don't have a lucky birthday. :(";
+
+imgHappy.style.display = "none"; //resetting the display state (if user gets lucky output first and then not lucky, the state is updated)
+imgSad.style.display = "block";
+
+}
+
+else {
+    output.innerHTML= "Please enter a valid input.";
+}
+}
 
 btnCheck.addEventListener("click", clickHandler);
 
-
-//07-05-2001
